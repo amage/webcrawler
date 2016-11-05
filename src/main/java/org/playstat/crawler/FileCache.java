ec787456d8f97d685e1eca366d71215bd70522fc
Iurii Shchekochikhin <amagex@gmail.com>
2016-11-06 02:06:42 +0300

    FileCache clean

--- src/main/java/org/playstat/crawler/FileCache.java
+++ src/main/java/org/playstat/crawler/FileCache.java
@@ -3,13 +3,21 @@ package org.playstat.crawler;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import org.playstat.agent.HTTPRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FileCache implements ICache {
-    private static final String CACHE_FOLDER = System.getProperty("user.home") + File.separator + ".parser" + File.separator
-            + "cache" + File.separator;
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
+    private static final String CACHE_FOLDER = String.join(File.separator, System.getProperty("user.home"), ".parser", "cache") + File.separator;
+
+    public FileCache() {
+        log.debug("Cache folder: " + CACHE_FOLDER);
+    }
     @Override
     public File getCacheFile(HTTPRequest request) {
         try {
@@ -17,19 +25,20 @@ public class FileCache implements ICache {
 			final String host = new URL(url).getHost();
             String filename = MD5(url);
             final File cacheFolder = prepareFS();
-            final String subFolder = host + File.separator + filename.substring(0, 2) + File.separator + filename.substring(0, 4);
+            final String subFolder = String.join(File.separator, host,
+                    filename.substring(0, 2), filename.substring(0, 4));
             filename = subFolder + File.separator + filename;
 
             new File(cacheFolder.getAbsolutePath() + File.separator + subFolder).mkdirs();
 
             return new File(cacheFolder.getAbsolutePath() + File.separator + filename);
 
-        } catch (MalformedURLException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    private File prepareFS() {
+    private static File prepareFS() {
         File cacheFolder = new File(CACHE_FOLDER);
         if (!cacheFolder.exists()) {
             cacheFolder.mkdirs();
@@ -37,19 +46,13 @@ public class FileCache implements ICache {
         return cacheFolder;
     }
 
-    private String MD5(String md5) {
-        try {
-            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
-            byte[] array = md.digest(md5.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < array.length; ++i) {
-                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
-            }
-            return sb.toString();
-        } catch (java.security.NoSuchAlgorithmException e) {
-            // TODO logger
-            e.printStackTrace();
+    private static String MD5(String md5) throws NoSuchAlgorithmException {
+        final MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+        final byte[] array = md.digest(md5.getBytes());
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; ++i) {
+            sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
         }
-        return null;
+        return sb.toString();
     }
 }
